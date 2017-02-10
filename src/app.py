527f666a22fe589941cb6250d3fadc0ca2a11745
Henry Shaw <henryshaw80@gmail.com>
2017-02-10 18:37:34 +0800

    add random number

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 __author__ = "Timur"
 
 from flask import Flask, render_template, request, session, make_response
+from random import randint
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -10,6 +11,24 @@ app.secret_key = "Timur"
 @app.route('/') # 127.0.0.1:5000/
 def home_template():
     return render_template('home.jinja2')
+
+@app.route('/bootstrap') # 127.0.0.1:5000/bootstrap
+def bootstrap_template():
+    # we need to render jinja2 language into something
+    # that a web browser understand
+
+    population = list(range(1, 11))
+    sample = []
+    count = 0
+    while (count < 10):
+        index = randint(0, 9)
+        if population[index] is not 0: # if we found an unmarked value
+            sample.append(population[index])
+            population[index] = 0 #mark selected
+            count = count + 1
+
+    #imgloc = "static/img/" + "clockimage.png"
+    return render_template('bootstrap.jinja2', sample=sample)
 
 
 if __name__ == '__main__':
